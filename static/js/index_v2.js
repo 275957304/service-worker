e91a4bb3de30706c426cb751824c0d34079a56cb
q275957304 <275957304@qq.com>
2018-11-26 14:17:41 +0800

    first commit

---
+++ static/js/index_v2.js
@@ -0,0 +1,6 @@
+$.ajax('/getList').done(function (res) {
+    res.data.forEach(item => {
+        $('#list').append(`<li>${item}</li>`)
+    })
+});
+console.log('>>>>>>>', 2);
---
+++ sw/sw.js
@@ -0,0 +1,54 @@
+const VERSION = 'v7';
+const CACHE_NAME = 'service-worker-demo' + VERSION;
+
+console.log('begin');
+
+this.addEventListener('install', function (event) {
+    console.log('安装 sw.js');
+    event.waitUntil(
+        caches.open(CACHE_NAME).then(function (cache) {
+            return cache.addAll([
+                './',
+                'getList',
+                'img/avatar_v1.jpg',
+                'js/index_v2.js',
+                'js/jquery_v1.js'
+            ]);
+        })
+    )
+});
+
+this.addEventListener('activate', function (event) {
+    console.log('激活 sw.js，可以开始处理 fetch 请求。');
+    event.waitUntil(
+        caches.keys().then(function (keyList) {
+            return Promise.all(keyList.map(function (key) {
+                if (CACHE_NAME.indexOf(key) === -1) {
+                    return caches.delete(key);
+                }
+            }))
+        })
+    )
+});
+
+this.addEventListener('fetch', function (event) {
+    event.respondWith(
+        caches.match(event.request)
+            .then(function (resp) {
+                if (resp) {
+                    console.log(new Date(), 'fetch ', event.request.url, '有缓存，从缓存中取');
+                    return resp;
+                } else {
+                    console.log(new Date(), 'fetch ', event.request.url, '没有缓存，网络获取');
+                    return fetch(event.request)
+                        .then(function (response) {
+                            return caches.open(CACHE_NAME).then(function (cache) {
+                                cache.put(event.request, response.clone());
+                                return response;
+                            })
+                        })
+                }
+            })
+    )
+});
+console.log('end');
